cc5eadac68b1a56d0872b1785b0b2021ba935b75
Aditi Prachi <aditiprachi21@gmail.com>
2021-07-14 00:24:24 +0530

    homepage main comp

--- client/src/components/Main/Main.js
+++ client/src/components/Main/Main.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import '../Main/Main.css'
 
 function Main() {
     return (
-        <div>
-            
+        <div className="main">
+            <h1 className="heading">Welcome to Target Synergy</h1>
+            <p className="tagline">Helps provide instant solutions on opinions, feedback and polls from your targeted audience as their opinion matters.</p>
         </div>
     )
 }
