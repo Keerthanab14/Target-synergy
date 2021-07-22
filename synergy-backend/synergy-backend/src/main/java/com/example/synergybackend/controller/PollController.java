4100c6186c3a7d0cca79390317fc999d52ca742e
keerthana <keerthanareddyboreddy@gmail.com>
2021-07-23 03:53:41 +0530

    Controller

--- synergy-backend/synergy-backend/src/main/java/com/example/synergybackend/controller/PollController.java
+++ synergy-backend/synergy-backend/src/main/java/com/example/synergybackend/controller/PollController.java
@@ -1,12 +1,16 @@
 package com.example.synergybackend.controller;
 
+import com.example.synergybackend.model.Choice;
 import com.example.synergybackend.model.Poll;
+import com.example.synergybackend.repo.CustRepo;
 import com.example.synergybackend.repo.PollRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
 
 @RestController
 //@RequestMapping("/api/polls")
