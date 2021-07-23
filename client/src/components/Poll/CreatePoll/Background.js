7ff5a034b75e394c7715661907ce02ae1e9ebea5
Shefali Shefali <shefali.bhpv@gmail.com>
2021-07-23 21:40:38 +0530

    adding background color and adding toggle button in polltype

--- client/src/components/Poll/CreatePoll/Background.js
+++ client/src/components/Poll/CreatePoll/Background.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 import Slider from '@material-ui/core/Slider';
@@ -8,7 +8,7 @@ import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 
 
-const Background = () => {
+const Background=(props) => {
     const useStyles = makeStyles((theme) => ({
         h:{
             fontSize: '15px',
@@ -36,7 +36,7 @@ const Background = () => {
     const handleSliderChange = (event, newValue) => {
       setValue(newValue);
     };
-  
+    
     const handleInputChange = (event) => {
       setValue(event.target.value === '' ? '' : Number(event.target.value));
     };
@@ -47,20 +47,28 @@ const Background = () => {
           setValue(100);
         }
       };
+        
+    
+      
+       const {parentCallback}=props;
+    
+
     return (
         <div >
+          
             <h4 className={classes.h5}>Style up your slide with colours and images! Your audience will also see the same background on their screen.</h4>
             <h4 className={classes.h}>Text Colour</h4>
-            <Button variant="contained" className={classes.button} style={{backgroundColor: 'white', border: '1px solid grey'}}></Button>
-            <Button variant="contained" className={classes.button} style={{backgroundColor: 'black', border: '1px solid grey'}}></Button>
+            <Button variant="contained" className={classes.button} style={{backgroundColor: 'white', border: '1px solid grey'}} ></Button>
+             
+            <Button variant="contained" className={classes.button} style={{backgroundColor: 'black', border: '1px solid grey'}} ></Button>
             <h4 className={classes.h}>Base Colour</h4>
-            <Button variant="contained" className={classes.button} style={{backgroundColor: 'white', border: '1px solid grey'}}></Button>
-            <Button variant="contained" className={classes.button} style={{backgroundColor: 'black'}}></Button>
-            <Button variant="contained" className={classes.button} style={{backgroundColor: '#99ddff' /* light blue */, border: '1px solid grey'}}></Button> 
-            <Button variant="contained" className={classes.button} style={{backgroundColor: '#ffff4d' /* light yellow */, border: '1px solid grey'}}></Button>
-            <Button variant="contained" className={classes.button} style={{backgroundColor: '#005580' /* blue */, border: '1px solid grey'}}></Button>
-            <Button variant="contained" className={classes.button} style={{backgroundColor: 'red', border: '1px solid grey'}}></Button>
-            <Button variant="contained" className={classes.button} style={{backgroundColor: 'brown', border: '1px solid grey'}}></Button>
+            <Button variant="contained" className={classes.button} style={{backgroundColor: 'white', border: '1px solid grey'}} onClick={()=>parentCallback("white")}></Button>
+            <Button variant="contained" className={classes.button} style={{backgroundColor: 'black'}} onClick={()=>parentCallback("black")}></Button>
+            <Button variant="contained" className={classes.button} style={{backgroundColor: '#99ddff' /* light blue */, border: '1px solid grey'}} onClick={()=>parentCallback("#99ddff")}></Button> 
+            <Button variant="contained" className={classes.button} style={{backgroundColor: '#ffff4d' /* light yellow */, border: '1px solid grey'}} onClick={()=>parentCallback("#ffff4d")}></Button>
+            <Button variant="contained" className={classes.button} style={{backgroundColor: '#005580' /* blue */, border: '1px solid grey'}} onClick={()=>parentCallback("#005580")}></Button>
+            <Button variant="contained" className={classes.button} style={{backgroundColor: 'red', border: '1px solid grey'}} onClick={()=>parentCallback("red")}></Button>
+            <Button variant="contained" className={classes.button} style={{backgroundColor: 'brown', border: '1px solid grey'}} onClick={()=>parentCallback("brown")} ></Button>
             <p style={{color:'slategrey', marginRight: '5px', marginBottom: '2px', fontSize: '13px'}}>Custom</p>
             <Button variant="contained" className={classes.button} style={{backgroundColor: 'white', border: '1px solid grey', marginTop: '5px'}}></Button>
             <h4 className={classes.h}>Background Image</h4>
