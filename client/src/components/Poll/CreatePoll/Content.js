4806c68a4b7061cbf82213414e17c3683e5e8ed3
unknown <avanshika2000@gmail.com>
2021-08-01 02:47:58 +0530

    Custom bg and tweeks

--- client/src/components/Poll/CreatePoll/Content.js
+++ client/src/components/Poll/CreatePoll/Content.js
@@ -114,24 +114,22 @@ const Content = () => {
           <form onSubmit={submit} className={classes.root} noValidate autoComplete="off"><h4 className={classes.h}>Your Question</h4>
       
         
-        <TextField id="outlined-basic" label="Your multiple choice question" variant="outlined" size="small" onChange={(e)=>handle(e)} id="question" value={data.question} style={{width: '96%'}}/>
+        <TextField id="outlined-basic" label="Your multiple choice question" variant="outlined" size="small" onChange={(e)=>handle(e)} id="question" value={data.question} style={{width: '100%'}}/>
         <h4 className={classes.h}>Options</h4>
-        <Grid container={true}  direction="row"  alignItems="center" 
->
-{inputList.map((x, i) => {
+        <Grid container={true}  direction="row"  alignItems="center" >
+          {inputList.map((x, i) => {
         return (
           <div className="box">
-          
             <TextField
             id="outlined-basic"
             variant="outlined" size="small"
             id="outlined-basic"
             style={{width: '96%'}}
-              className="ml10"
-              name="choice"
-   placeholder="Enter choice"
-              value={x.choice}
-              onChange={e => handleInputChange(e, i)}
+            className="ml10"
+            name="choice"
+            placeholder="Enter choice"
+            value={x.choice}
+            onChange={e => handleInputChange(e, i)}
             />
             <div>
               {inputList.length !== 1 && <DeleteIcon style={{background:"#cc0000", color:"white", marginRight:"3px"}}
@@ -144,7 +142,7 @@ const Content = () => {
       })}
     
       </Grid>  <Button
-        style={{ width: "248px",background:"#cc0000", color:"white" }}
+        style={{ width: "235px",background:"#cc0000", color:"white" }}
         className={classes.button}
         variant="contained"
        // color="primary"
