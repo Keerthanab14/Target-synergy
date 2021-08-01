2c740f09a2343aafdc5aadb1efd9f511474b43ad
Aditi Prachi <aditiprachi21@gmail.com>
2021-08-01 14:30:30 +0530

    content render

--- client/src/components/Poll/CreatePoll/Sidebar.js
+++ client/src/components/Poll/CreatePoll/Sidebar.js
@@ -5,6 +5,7 @@ import PropTypes from 'prop-types';
 import SwipeableViews from 'react-swipeable-views';
 import PollType from '../CreatePoll/PollType'
 import ContentQandA from '../CreatePoll/ContentQandA'
+import Content from '../CreatePoll/Content'
 import Background from '../CreatePoll/Background'
 import '../CreatePoll/CreatePolls.css'
 import AppBar from '@material-ui/core/AppBar';
@@ -60,9 +61,10 @@ function a11yProps(index) {
 
 
 
-const Sidebar = ({ parentCallback, clickHandler, setOpacity, togglePopup }) => {
+const Sidebar = ({ parentCallback, setOpacity, togglePopup }) => {
   const theme = useTheme();
   const [value, setValue] = React.useState(0);
+  const [type, setType] = React.useState("");
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
@@ -72,6 +74,9 @@ const Sidebar = ({ parentCallback, clickHandler, setOpacity, togglePopup }) => {
     setValue(index);
   };
   
+  const clickHandler = (pType)=>{
+    setType(pType)
+ }
    
    const classes = useStyles();
     return (
@@ -100,7 +105,7 @@ const Sidebar = ({ parentCallback, clickHandler, setOpacity, togglePopup }) => {
           clickHandler={clickHandler} style={{flex: '1'}}  />
         </TabPanel>
         <TabPanel value={value} index={1} dir={theme.direction}style={{flex: '1'}}>
-          <ContentQandA />
+        {(type==="multiplechoice") ? <Content/> : <ContentQandA/>}
         </TabPanel>
         <TabPanel value={value} index={2} dir={theme.direction}style={{flex: '1'}}>
           <Background parentCallback={parentCallback} setOpacity={setOpacity} togglePopup={togglePopup}/>
