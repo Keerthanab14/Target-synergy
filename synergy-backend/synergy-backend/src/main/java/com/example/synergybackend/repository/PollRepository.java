3474e5e720a3249a73c9d7d5c4bcbcebb1f716f1
Aditi Prachi <aditiprachi21@gmail.com>
2021-08-05 10:43:19 +0530

    display last saved

--- synergy-backend/synergy-backend/src/main/java/com/example/synergybackend/repository/PollRepository.java
+++ synergy-backend/synergy-backend/src/main/java/com/example/synergybackend/repository/PollRepository.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
 @Repository
-public interface PollRepository extends  MongoRepository<Poll, Long>{
+public interface PollRepository extends  MongoRepository<Poll, String>{
 
 
 }
