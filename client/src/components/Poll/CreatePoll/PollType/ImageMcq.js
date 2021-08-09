c72bf5964ff7f786e17d5f29253553db34056341
keerthana <keerthanareddyboreddy@gmail.com>
2021-08-10 02:47:20 +0530

    image changes

--- client/src/components/Poll/CreatePoll/PollType/ImageMcq.js
+++ client/src/components/Poll/CreatePoll/PollType/ImageMcq.js
@@ -1,53 +1,14 @@
-import React, { useState,useEffect,useContext } from "react";
-import Poll from "react-polls";
-import axios from "axios";
-import { IdContext } from '../../../../IdContext';
-import CardMedia from '@material-ui/core/CardMedia';
-import feedback from '../../../images/feedback_1.png'
+import { KeyboardReturn } from "@material-ui/icons";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "../../../../imageMcq/App/App";
 
-// Declaring poll question and answers
- 
-const ImageMcq = () => {
-  const [textBased , setTextBased] = useState({question:'', choices:[]})
-  const id = useContext(IdContext);
-  const url = id.id;
-    useEffect(async () => {
-      var result = await axios.get(`http://localhost:8080/mcq/610fbf5066e210524c8325a5`)
-      console.log(result)
-      setTextBased({
-        question: result.data.question,
-        choices: result.data.choices 
-      })
-    },[])
-    const choice=[];
-    textBased.choices.map((post,key) => (  
-        choice[key]=({option:<img src={axios.get(`http://localhost:8080/file/download/${post.text}`)}/>, votes:0})
-    
-    ));
-     
-// console.log(choice);
-// console.log(id.id);
-let Answers = [...choice];
-console.log(Answers);
-  const pollQuestion = textBased.question;
-  const handleVote = (voteAnswer) => {
-    const newAnswers = Answers.map((answer) => {
-      if (answer.option === voteAnswer) {
-          answer.votes++;
-      }
-      console.log(answer);
-      return answer;
-      
-    });
-    Answers= newAnswers;
-  };
- 
-  return (
-    <div>
-      <Poll question={pollQuestion} answers={Answers} onVote={handleVote} />
-     
-    </div>
-  );
-};
-
+function ImageMcq() {
+return(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+}
 export default ImageMcq;
