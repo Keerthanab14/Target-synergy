25f4324300eaaf7bf883c663ff7a0f440040ea29
Keerthana B <60786451+Keerthanab14@users.noreply.github.com>
2021-08-11 02:05:59 +0530

    Delete synergy-backend directory

--- synergy-backend/synergy-backend/src/main/java/com/example/synergybackend/repository/MCQRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.synergybackend.repository;
-
-import com.example.synergybackend.model.Mcq;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface MCQRepository extends MongoRepository<Mcq, String> {
-}
--- synergy-backend/synergy-backend/src/main/java/com/example/synergybackend/services/PollService.java
+++
@@ -1,4 +0,0 @@
-package com.example.synergybackend.services;
-
-public class PollService {
-}
