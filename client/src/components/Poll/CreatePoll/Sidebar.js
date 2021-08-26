bf6eaa8f6cc3e721b6b3e9179b0daaa805e691d2
Shefali Shefali <shefali.bhpv@gmail.com>
2021-08-26 23:24:38 +0530

    mcc more than one option

--- client/src/components/Poll/CreatePoll/Sidebar.js
+++ client/src/components/Poll/CreatePoll/Sidebar.js
@@ -70,7 +70,7 @@ function a11yProps(index) {
 
 
 const Sidebar = ({ parentCallback, color, textcolor, clickHandler, setOpacity, togglePopup, component, setData1, data1, data2, setData2, data3, setData3, setData5, data5, 
-data6, setData6, data4, setData4, setResult, result, inputListMC, inputListRanking,setInputListRanking, setInputListMC, setState, state, inputListScales, setInputListScales, settextcolor, contentauth, imgresult, setImgResult, img, setUri,opacity, BackgroundImage }) => {
+data6, setData6, data4, setData4, setResult, result, inputListMC, inputListRanking,setInputListRanking, setInputListMC, setState, state, inputListScales, setInputListScales, settextcolor, contentauth,opacity, BackgroundImage }) => {
 
   const theme = useTheme();
   const [value, setValue] = React.useState(0);
@@ -118,7 +118,7 @@ data6, setData6, data4, setData4, setResult, result, inputListMC, inputListRanki
         {component==="qanda" && <ContentQandA QandA={data6} setQandA={setData6} handleChangeIndex={handleChangeIndex} />}
         {component==="wordcloud" && <ContentWordCloud data4={data4} setData4={setData4} handleChangeIndex={handleChangeIndex}/>}
         {component==="openended" && <ContentOpenEnded setOpenEnded={setData3} OpenEnded={data3} handleChangeIndex={handleChangeIndex}/>}
-       {component==="ranking" && <ContentRanking setData2={setData2} data2={data2} inputList={inputListRanking} setInputList={setInputListRanking} handleChangeIndex={handleChangeIndex} setState={setState} state={state} image={img} setUri={setUri}/>} 
+       {component==="ranking" && <ContentRanking setData2={setData2} data2={data2} inputList={inputListRanking} setInputList={setInputListRanking} handleChangeIndex={handleChangeIndex} />} 
        {component==="scales" && <ContentScales data5={data5} setData5={setData5} setResult={setResult} result={result} inputList={inputListScales} setInputList={setInputListScales} handleChangeIndex={handleChangeIndex} contentauth={contentauth}/>}
 
         </TabPanel>
