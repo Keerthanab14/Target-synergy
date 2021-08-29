448108ca0560996e2aace96d36dc8ccf090572b3
Keerthanab14 <keerthanareddyboreddy@gmail.com>
2021-08-30 01:40:50 +0530

    OpenEnded Unit Testing

---
+++ micro services/openEnded/src/test/java/com/synergy/openEnded/model/OpenEndedTest.java
@@ -0,0 +1,13 @@
+package com.synergy.openEnded.model;
+
+import org.junit.jupiter.api.BeforeEach;
+
+class OpenEndedTest {
+
+    private OpenEnded openEndedUnderTest;
+
+    @BeforeEach
+    void setUp() {
+        openEndedUnderTest = new OpenEnded();
+    }
+}
