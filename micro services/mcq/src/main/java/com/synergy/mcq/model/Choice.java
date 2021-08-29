18878c17a4d65f220832e6e6cc986e92538ca357
Aditi Prachi <aditiprachi21@gmail.com>
2021-08-29 10:40:36 +0530

    micro services

---
+++ micro services/mcq/src/main/java/com/synergy/mcq/model/Choice.java
@@ -0,0 +1,28 @@
+package com.synergy.mcq.model;
+
+public class Choice {
+
+    private String option;
+    private int votes;
+
+    public Choice(String option, int votes) {
+        this.option = option;
+        this.votes = votes;
+    }
+
+    public String getOption() {
+        return option;
+    }
+
+    public void setOption(String option) {
+        this.option = option;
+    }
+
+    public int getVotes() {
+        return votes;
+    }
+
+    public void setVotes(int votes) {
+        this.votes = votes;
+    }
+}
