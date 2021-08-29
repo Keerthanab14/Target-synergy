5dbf61c8c2a362daf85ab2b80ddb74a6bc7649f6
Keerthanab14 <keerthanareddyboreddy@gmail.com>
2021-08-30 01:05:48 +0530

    MCQ  Unit testing

---
+++ micro services/mcq/src/test/java/com/synergy/mcq/model/McqTest.java
@@ -0,0 +1,13 @@
+package com.synergy.mcq.model;
+
+import org.junit.jupiter.api.BeforeEach;
+
+class McqTest {
+
+    private Mcq mcqUnderTest;
+
+    @BeforeEach
+    void setUp() {
+        mcqUnderTest = new Mcq();
+    }
+}
